44aad67b8a82507683c90fa6c6bbad034cc25ca5
AWS GIT stakeholder <skrambletoe@gmail.com>
2022-02-04 17:25:49 +0200

    Revert "Testing"
    This reverts commit 20b164c940576aa9dabe60063cd7a43316d76e5c.

---
+++ chromedriver/main.py
@@ -0,0 +1,63 @@
+import telebot
+from auth_data import token
+from auth_data import bet_pass
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+service = Service("C:\\Users\\krabs\\Downloads\\govno_slona\\govno_slona\\chromedriver\\chromedriver.exe")
+url = "https://skladbet.com/login/"
+url2 = "https://skladbet.com/threads/26-10-21.5338/"
+driver = webdriver.Chrome(service=service)
+
+def telegram_bot(token):
+    bot = telebot.TeleBot(token)
+
+    @bot.message_handler(commands=["start"])
+    def start_message(message):
+        bot.send_message(message.chat.id, "Batko nash bandera")
+
+    @bot.message_handler(content_types=["text"])
+    def send_text(message):
+        login = False
+        while message.text == "Ukraina matu":
+            try:
+                while not login:
+                    driver.get(url=url)
+
+                    email_input = driver.find_element(By.NAME, "login")
+                    email_input.clear()
+                    email_input.send_keys("telebot@gmail.com")
+
+                    password_input = driver.find_element(By.NAME, "password")
+                    password_input.clear()
+                    password_input.send_keys(bet_pass)
+
+                    password_input.send_keys(Keys.ENTER)
+                    time.sleep(1)
+                    login = True
+
+                driver.get(url2)
+                items = driver.find_elements(By.CLASS_NAME, "bbWrapper")
+
+                # with open("project.txt", "w") as file:
+                #     for i in range(0, len(items)):
+                #         file.write(items[i].text)
+
+                for i in range(0, len(items)):
+                    bot.send_message(message.chat.id, items[i].text)
+                time.sleep(30)
+            except Exception as ex:
+                print(ex)
+                bot.send_message(message.chat.id, "Pomulka")
+
+        else:
+            bot.send_message(message.chat.id, "Moskal?")
+
+    bot.polling()
+
+if __name__ == '__main__':
+    # parser()
+    telegram_bot(token)
